c73dd9150c77bed7279c61fc798e10cfcfa4a23a
Joe Fowler <scrumplesplunge@gmail.com>
2025-12-01 21:03:10 +0000

    190us cleanish solution

--- src/day01.c
+++ src/day01.c
@@ -1,3 +1,17 @@
+// Counting times where a dial hits zero:
+//
+//   Part 1: inbetween distinct moves.
+//   Part 2: at any point during a move.
+//
+// This is a typical modular arithmetic puzzle, but there are several edge cases
+// to consider, particularly regarding cases where a move starts or ends on 0.
+// This solution simplifies the tracking of zero crossings by:
+//
+//   * Turning any 0 into a 100 at the end of a move.
+//   * Focusing on left turns, and handling right turns by inverting the dial.
+//     This flipped state is tracked by a parity field: 'L' means unflipped,
+//     'R' means flipped.
+
 #include "core/assert.h"
 #include "core/io.h"
 
@@ -9,6 +23,7 @@ int main() {
   }
   const char* i = buffer;
   const char* const end = buffer + length;
+  char parity = 'L';
   int dial = 50;
   unsigned int zeros = 0;
   unsigned int passes = 0;
@@ -20,23 +35,20 @@ int main() {
     if (!i || (d != 'L' && d != 'R')) die("bad input t");
     const int q = n / 100, r = n % 100;
     passes += q;
-    if (d == 'L') {
-      if (r <= dial) {
-        dial -= r;
-        if (dial == 0) passes++;
-      } else {
-        if (dial != 0) passes++;
-        dial -= r - 100;
-      }
+    if (d != parity) {
+      parity = d;
+      dial = 100 - dial;
+    }
+    if (dial == 0 || dial == 100) {
+      dial = 100 - r;
     } else {
-      if (r < 100 - dial) {
-        dial += r;
-      } else {
-        if (dial != 0) passes++;
-        dial += r - 100;
+      dial -= r;
+      if (dial == 0) zeros++;
+      if (dial <= 0) {
+        passes++;
+        dial += 100;
       }
     }
-    if (dial == 0) zeros++;
   }
   print("%d\n%d\n", zeros, passes);
 }
