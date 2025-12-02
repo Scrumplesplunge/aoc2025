8213d101f6cf32ca36f1fa8d00bb61d1977ccc71
Joe Fowler <scrumplesplunge@gmail.com>
2025-12-02 17:43:12 +0000

    Implement (u)int64 support.

--- src/core/io.h
+++ src/core/io.h
@@ -6,15 +6,27 @@
 
 void die(const char*);
 
-char* format_uint(char* restrict out, unsigned int x) {
-  char buffer[16];
-  int i = 16;
+char* format_uint(char* restrict out, unsigned x) {
+  char buffer[10];
+  int i = 10;
   do {
     --i;
     buffer[i] = '0' + (x % 10);
     x /= 10;
   } while (x);
-  while (i < 16) *out++ = buffer[i++];
+  while (i < 10) *out++ = buffer[i++];
+  return out;
+}
+
+char* format_ulong(char* restrict out, unsigned long long x) {
+  char buffer[24];
+  int i = 24;
+  do {
+    --i;
+    buffer[i] = '0' + (x % 10);
+    x /= 10;
+  } while (x);
+  while (i < 24) *out++ = buffer[i++];
   return out;
 }
 
@@ -26,8 +38,8 @@ char* format_string(char* restrict out, const char* restrict s) {
 char* vformat(char* restrict out, const char* restrict pattern, va_list args) {
   while (pattern) {
     while (*pattern != '\0' && *pattern != '%') *out++ = *pattern++;
-    if (*pattern == '\0') break;
-    switch (pattern[1]) {
+    if (*pattern++ == '\0') break;
+    switch (*pattern++) {
       case '%':
         *out++ = '%';
         break;
@@ -45,7 +57,27 @@ char* vformat(char* restrict out, const char* restrict pattern, va_list args) {
         break;
       }
       case 'u':
-        out = format_uint(out, va_arg(args, unsigned int));
+        out = format_uint(out, va_arg(args, unsigned long long));
+        break;
+      case 'l':
+        if (*pattern++ != 'l') die("bad format");
+        switch (*pattern++) {
+          case 'd': {
+            const long long value = va_arg(args, long long);
+            if (value < 0) {
+              *out++ = '-';
+              out = format_ulong(out, -value);
+            } else {
+              out = format_ulong(out, value);
+            }
+            break;
+          }
+          case 'u':
+            out = format_ulong(out, va_arg(args, unsigned long long));
+            break;
+          default:
+            die("bad format");
+        }
         break;
       case 's':
         out = format_string(out, va_arg(args, const char*));
@@ -53,11 +85,11 @@ char* vformat(char* restrict out, const char* restrict pattern, va_list args) {
       default:
         die("bad format");
     }
-    pattern += 2;
   }
   return out;
 }
 
+__attribute__((format(printf, 2, 3)))
 char* format(char* restrict out, const char* restrict pattern, ...) {
   va_list args;
   va_start(args, pattern);
@@ -89,6 +121,17 @@ void print_uints(unsigned int a, unsigned int b) {
   write(STDOUT_FILENO, buffer, o - buffer);
 }
 
+void print_ulongs(unsigned long long a, unsigned long long b) {
+  char buffer[128];
+  char* o = buffer;
+  o = format_ulong(o, a);
+  *o++ = '\n';
+  o = format_ulong(o, b);
+  *o++ = '\n';
+  write(STDOUT_FILENO, buffer, o - buffer);
+}
+
+__attribute__((format(printf, 1, 2)))
 void print(const char* restrict pattern, ...) {
   va_list args;
   va_start(args, pattern);
@@ -111,14 +154,22 @@ const char* scan_uint(const char* restrict in, unsigned int* x) {
   return in;
 }
 
+const char* scan_ulong(const char* restrict in, unsigned long long* x) {
+  if (!is_digit(*in)) return NULL;
+  unsigned int value = 0;
+  while (is_digit(*in)) value = 10 * value + (*in++ - '0');
+  *x = value;
+  return in;
+}
+
 const char* vscan(const char* restrict in, const char* restrict pattern,
                   va_list args) {
   while (pattern) {
     while (*pattern != '\0' && *pattern != '%') {
       if (*in++ != *pattern++) return NULL;
     }
-    if (*pattern == '\0') break;
-    switch (pattern[1]) {
+    if (*pattern++ == '\0') break;
+    switch (*pattern++) {
       case '%':
         if (*in++ != '%') return NULL;
         break;
@@ -143,6 +194,31 @@ const char* vscan(const char* restrict in, const char* restrict pattern,
         in = scan_uint(in, va_arg(args, unsigned int*));
         if (!in) return NULL;
         break;
+      case 'l':
+        if (*pattern++ != 'l') die("bad format");
+        switch (*pattern++) {
+          case 'd': {
+            if (*in == '-') {
+              unsigned long long x;
+              in = scan_ulong(in, &x);
+              if (!in) return NULL;
+              *va_arg(args, long long*) = -x;
+            } else {
+              unsigned long long x;
+              in = scan_ulong(in, &x);
+              if (!in) return NULL;
+              *va_arg(args, long long*) = x;
+            }
+            break;
+          }
+          case 'u':
+            in = scan_ulong(in, va_arg(args, unsigned long long*));
+            if (!in) return NULL;
+            break;
+          default:
+            die("bad format");
+        }
+        break;
       default:
         die("bad format");
     }
@@ -151,6 +227,7 @@ const char* vscan(const char* restrict in, const char* restrict pattern,
   return in;
 }
 
+__attribute__((format(scanf, 2, 3)))
 const char* scan(const char* restrict in, const char* restrict pattern, ...) {
   va_list args;
   va_start(args, pattern);
