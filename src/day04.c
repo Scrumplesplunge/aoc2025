1ef5ca6401dce2dc345e9891d3b0ca552b10dc67
Joe Fowler <scrumplesplunge@gmail.com>
2025-12-04 06:14:44 +0000

    Day 4 part 2.

--- src/day04.c
+++ src/day04.c
@@ -2,7 +2,7 @@
 #include "core/io.h"
 
 enum { max_size = 142 };
-bool grid[max_size + 2][max_size + 2];
+bool grid[2][max_size + 2][max_size + 2];
 
 int main() {
   char buffer[20 << 10];
@@ -22,25 +22,42 @@ int main() {
     for (int x = 0; x < width; x++) {
       const char cell = buffer[y * (width + 1) + x];
       if (cell != '.' && cell != '@') die("bad input");
-      grid[y + 1][x + 1] = (cell == '@');
+      grid[0][y + 1][x + 1] = (cell == '@');
     }
   }
 
-  // Solve part 1.
+  int iteration = 0;
   unsigned part1 = 0;
-  for (int y = 1; y <= height; y++) {
-    for (int x = 1; x <= width; x++) {
-      if (!grid[y][x]) continue;
-      int adjacent = 0;
-      for (int dy = -1; dy <= 1; dy++) {
-        for (int dx = -1; dx <= 1; dx++) {
-          if (dx == 0 && dy == 0) continue;
-          if (grid[y + dy][x + dx]) adjacent++;
+  unsigned part2 = 0;
+  while (true) {
+    const bool (*from)[max_size + 2] = grid[iteration % 2];
+    bool (*to)[max_size + 2] = grid[(iteration + 1) % 2];
+
+    unsigned num_removed = 0;
+    for (int y = 1; y <= height; y++) {
+      for (int x = 1; x <= width; x++) {
+        to[y][x] = from[y][x];
+        if (!from[y][x]) continue;
+        int adjacent = 0;
+        for (int dy = -1; dy <= 1; dy++) {
+          for (int dx = -1; dx <= 1; dx++) {
+            if (dx == 0 && dy == 0) continue;
+            if (from[y + dy][x + dx]) adjacent++;
+          }
+        }
+        if (adjacent < 4) {
+          to[y][x] = false;
+          num_removed++;
         }
       }
-      if (adjacent < 4) part1++;
     }
+
+    if (num_removed == 0) break;
+    if (iteration == 0) part1 += num_removed;
+    part2 += num_removed;
+    iteration++;
+    if (iteration == max_size) die("too long");
   }
 
-  print_uints(part1, 0);
+  print_uints(part1, part2);
 }
