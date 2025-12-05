e3c816b9ffa7857ad04782473df68d808879af4b
Joe Fowler <scrumplesplunge@gmail.com>
2025-12-05 06:42:09 +0000

    Day 5 part 2.

--- src/day05.c
+++ src/day05.c
@@ -6,10 +6,48 @@ struct inclusive_range {
 };
 
 enum { max_ranges = 200, max_ingredients = 1000 };
+
+// `ranges` is a list of non-overlapping, non-consecutive (so the end of one
+// range is not consecutive with the start of the next) ranges of fresh
+// ingredients.
 int num_ranges;
 struct inclusive_range ranges[max_ranges];
 int num_ingredients;
 unsigned long long ingredients[max_ingredients];
+
+void add_fresh_range(struct inclusive_range range) {
+  // Find the range of ranges which overlap or concatenate with `range`.
+  const int n = num_ranges;
+  int f = 0;
+  while (f < n && ranges[f].last + 1 < range.first) f++;
+  int l = f;
+  while (l < n && ranges[l].first < range.last + 1) l++;
+
+  if (f == n) {
+    // Strictly after all existing ranges.
+    if (num_ranges == max_ranges) die("too large");
+    ranges[num_ranges++] = range;
+  } else if (l == n) {
+    // Range is last, but overlaps with some.
+    ranges[f].last = range.last;
+    num_ranges = f + 1;
+  } else if (f == l) {
+    // Range inserts at f.
+    if (num_ranges == max_ranges) die("too large");
+    memmove(ranges + f + 1, ranges + f,
+            (n - f) * sizeof(struct inclusive_range));
+    ranges[f] = range;
+    num_ranges++;
+  } else {
+    // Range overlaps or concatenates with some entries.
+    if (ranges[f].first < range.first) range.first = ranges[f].first;
+    if (ranges[l - 1].last > range.last) range.last = ranges[l - 1].last;
+    memmove(ranges + f + 1, ranges + l,
+            (n - l) * sizeof(struct inclusive_range));
+    ranges[f] = range;
+    num_ranges -= l - f - 1;
+  }
+}
 
 void read_input() {
   char buffer[22 << 10];
@@ -21,12 +59,12 @@ void read_input() {
   const char* const end = buffer + length;
   // Scan the fresh ranges.
   while (*i != '\n') {
-    if (num_ranges == max_ranges) die("too large");
-    struct inclusive_range* range = &ranges[num_ranges++];
-    i = scan_ulong(i, &range->first);
+    struct inclusive_range range;
+    i = scan_ulong(i, &range.first);
     if (!i || *i++ != '-') die("bad input");
-    i = scan_ulong(i, &range->last);
+    i = scan_ulong(i, &range.last);
     if (!i || *i++ != '\n') die("bad input");
+    add_fresh_range(range);
   }
   // Scan the ingredient IDs.
   i++;
@@ -55,5 +93,11 @@ int main() {
     if (is_fresh(ingredients[i])) part1++;
   }
 
-  print_uints(part1, 0);
+  unsigned long long part2 = 0;
+  const int r = num_ranges;
+  for (int i = 0; i < r; i++) {
+    part2 += ranges[i].last - ranges[i].first + 1;
+  }
+
+  print_ulongs(part1, part2);
 }
