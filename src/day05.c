e2103f818ea8d2f1a755cb6f11bee1c4dba68a5f
Joe Fowler <scrumplesplunge@gmail.com>
2025-12-05 22:14:38 +0000

    Reduce size by making the range non-inclusive.

--- src/day05.c
+++ src/day05.c
@@ -1,7 +1,7 @@
 #include "core/assert.h"
 #include "core/io.h"
 
-struct inclusive_range {
+struct range {
   unsigned long long first, last;
 };
 
@@ -11,17 +11,17 @@ enum { max_ranges = 200, max_ingredients = 1000 };
 // range is not consecutive with the start of the next) ranges of fresh
 // ingredients.
 int num_ranges;
-struct inclusive_range ranges[max_ranges];
+struct range ranges[max_ranges];
 int num_ingredients;
 unsigned long long ingredients[max_ingredients];
 
-void add_fresh_range(struct inclusive_range range) {
+void add_fresh_range(struct range range) {
   const int n = num_ranges;
   int j = 0;
   // Remove any overlapping or adjacent ranges and add them to `range`.
   for (int i = 0; i < n; i++) {
-    struct inclusive_range* r = &ranges[i];
-    if (range.last + 1 >= r->first && r->last + 1 >= range.first) {
+    struct range* r = &ranges[i];
+    if (range.last >= r->first && r->last >= range.first) {
       if (r->first < range.first) range.first = r->first;
       if (r->last > range.last) range.last = r->last;
     } else {
@@ -48,11 +48,12 @@ void read_input() {
   const char* const end = buffer + length;
   // Scan the fresh ranges.
   while (*i != '\n') {
-    struct inclusive_range range;
+    struct range range;
     i = scan_ulong(i, &range.first);
     if (!i || *i++ != '-') die("bad input");
     i = scan_ulong(i, &range.last);
     if (!i || *i++ != '\n') die("bad input");
+    range.last++;
     add_fresh_range(range);
   }
   // Scan the ingredient IDs.
@@ -65,12 +66,12 @@ void read_input() {
 }
 
 bool is_fresh(unsigned long long id) {
-  const struct inclusive_range* i = ranges;
+  const struct range* i = ranges;
   int length = num_ranges;
   while (length > 0) {
     const int r = length % 2;
     length /= 2;
-    i += (i[length].last < id) * (length + r);
+    i += (i[length].last <= id) * (length + r);
   }
   return i->first <= id && id <= i->last;
 }
@@ -87,7 +88,7 @@ int main() {
   unsigned long long part2 = 0;
   const int r = num_ranges;
   for (int i = 0; i < r; i++) {
-    part2 += ranges[i].last - ranges[i].first + 1;
+    part2 += ranges[i].last - ranges[i].first;
   }
 
   print_ulongs(part1, part2);
